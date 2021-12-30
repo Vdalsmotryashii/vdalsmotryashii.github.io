c3937e356bb8cbafcd32e045fe6777f47aa5c3c8
Germann <93329384+Vdalsmotryashii@users.noreply.github.com>
2021-12-30 21:57:59 +0300

    исправлены ошибки
    исправлено или доделано: 
    -анимация появления абзаца
    -анимация цифр
    -анимацмя заголовков
    -подсветка пунктов меню
    -якорные ссылки
    -слайдер в кейсах + открытие в окне
    -гугл карты поменял на яндекс карты
    -телефон, мыло, гео-позиция кликабельны

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -4,30 +4,32 @@ new WOW().init();
 
 
 //magnific-popup
-// $(document).ready(function() {
-  // $('.image-link').magnificPopup({
-  	// type:'image',
-  	// closeOnContentClick: true,
-  // });
-
-// });
+ $(document).ready(function() {
+  $('.image-link').magnificPopup({
+  	type:'image',
+  	 closeOnContentClick: true,
+   });
+ });
+$('.test-popup-link').magnificPopup({
+  type: 'image'
+  // other options
+});
 
 
 
 
 //плавный скролл
-//		$(document).ready(function(){
-//			$("#menuID").on("click","a", function (event) {
-//				event.preventDefault();
+		$(document).ready(function(){
+			$("#menuID").on("click","a", function (event) {
+				event.preventDefault();
 
-//				var id  = $(this).attr('href'),
+				var id  = $(this).attr('href'),
 
-//					top = $(id).offset().top;
+					top = $(id).offset().top;
 				
-//				$('body,html').animate({scrollTop: top}, 1000);
-//			});
-//		})
-
+				$('body,html').animate({scrollTop: top}, 1500);
+			});
+		});
 
 
 
@@ -51,25 +53,42 @@ function onEntry (enrty){
 
 
 // подсветка меню при скроле
-//$(document).ready(function(){
+$(document).ready(function(){
 
-//	$(window).scroll(() => {
-//  	let scrollDistance = $(window).scrollTop();
-//		$(".section").each((i, el) => {
-//			if($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
-//				$("nav a").each((i, el) => {
-//					if ($(el).hasClass("active")){
-//						$(el).removeClass("active");
-//					}
-//					});
-//				$('nav li:eq('+ i +')').find('a').addClass('active');
-//				}
-//		});
-//	});
-//	});
-
+	$(window).scroll(() => {
+  	let scrollDistance = $(window).scrollTop();
+		$(".section").each((i, el) => {
+			if($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
+				$("nav a").each((i, el) => {
+				if ($(el).hasClass("active2")){
+						 $(el).removeClass("active2");
+					}
+				});
+				 $('nav li:eq('+ i +')').find('a').addClass('active2');
+			}	});
+ });
+ });
 
 
-
-
-
+//animation number
+$(document).ready(function () {
+ 
+    var show = true;
+    var countbox = ".benefits__inner";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; 
+        var w_top = $(window).scrollTop(); 
+        var e_top = $(countbox).offset().top; 
+        var w_height = $(window).height(); 
+        var d_height = $(document).height();
+        var e_height = $(countbox).outerHeight();
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.benefits__number').css('opacity', '1');
+            $('.benefits__number').spincrement({
+                thousandSeparator: "",
+                duration: 1200
+            });
+            show = false;
+        }
+    });
+});
