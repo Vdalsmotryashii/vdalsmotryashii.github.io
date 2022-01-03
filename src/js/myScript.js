2218baa40f11202514cd9bd34ec17b87a2ced10e
Germann <93329384+Vdalsmotryashii@users.noreply.github.com>
2022-01-03 21:12:12 +0300

    готов к хостингу
    вроде подтянул все хвосты по дз

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,5 +1,6 @@
 "use strict"
 
+//wow
 new WOW().init();
 
 
@@ -18,7 +19,7 @@ $('.test-popup-link').magnificPopup({
 
 
 
-//плавный скролл
+//плавный скролл - nicht arbaiten, Scheisse
 		$(document).ready(function(){
 			$("#menuID").on("click","a", function (event) {
 				event.preventDefault();
@@ -91,4 +92,92 @@ $(document).ready(function () {
             show = false;
         }
     });
+});
+
+
+//text-slider
+
+var text_slide_cur=0;
+function showtext_slide(){
+    $('#text_slide'+(text_slide_cur+1)).css({opacity: 0}).animate({opacity: 1.0,left: "50px"}, 1000);
+    setTimeout(hidetext_slide, 3000);
+}
+function hidetext_slide(){
+    $('#text_slide'+(text_slide_cur+1)).css({opacity: 1}).animate({opacity: 0,left: "-50px"}, 1000,function(){showtext_slide();});
+    text_slide_cur=(text_slide_cur+1)%5;
+}
+$(document).ready(function() {
+    showtext_slide();
+})
+
+
+ //Калькулятор
+ let v1 = 0;
+ let v2 = 0;
+ let v3 = 0;
+
+$(document).ready(function(){
+$('#js-button').click(function(){
+    
+	let value1 = $('#siteR').val();
+	let v1 = Number(value1);
+	let value2 = $('#designR').val();
+	let v2 = Number(value2);
+	let value3 = $('#adaptabilityR').val();
+	let v3 = Number(value3);
+
+	$('#js-result').html(v1 + v2 + v3 + ' у.е.'); 
+    console.log(v1, v2, v3)
+});
+});
+
+$(document).ready(function(){
+$('#js-button').click(function(){
+    let t = 0;
+
+    let value1 = $('#siteR').val();
+
+        if (value1 == '100'){
+            t = 1;
+        }
+        else if (value1 == '200'){
+            t = 2;
+        }
+        else if (value1 == '300'){
+            t = 3;
+        };
+       
+        console.log(t);
+
+//        
+    let value2 = $('#designR').val();
+        if (value2 == '50'){
+            t += 0;
+        }
+        else if (value2 == '60'){
+            t += 2;
+        }
+        else if (value2 == '70'){
+            t += 3;
+        };
+       
+        console.log(t)
+ //       
+    let value3 = $('#adaptabilityR').val();
+    
+        if (value3 == '0'){
+            t += 0;
+        }
+        else if (value3 == '10'){
+            t += 1;
+        };
+        console.log(t);
+
+if (t == 0){
+            alert("вы не выбрали пункт");
+        };
+        
+    $('#js-result2').html(t + ' д.');
+
+});
 });
--- src/js/myScryptTEMP.js
+++ src/js/myScryptTEMP.js
@@ -1,110 +1,75 @@
 "use strict"
 
 
-//let someFunction;
-//if (true) {
-//	someFunction = function(){
-//		console.log("its work");
-//		return;
-//	}
-//}
-//console.log(someFunction());
 
+//Калькулятор
+ let v1 = 0;
+ let v2 = 0;
+ let v3 = 0;
 
-
-//анимация цифр
-//$(document).ready(function(){
-//    $('.benefits__number').spincrement({
-//        thousandSeparator: "",
-//        duration: 1200
-//    });
-//});
-
-
-
+$(document).ready(function(){
 $('#js-button').click(function(){
+    
 	let value1 = $('#siteR').val();
 	let v1 = Number(value1);
 	let value2 = $('#designR').val();
 	let v2 = Number(value2);
 	let value3 = $('#adaptabilityR').val();
 	let v3 = Number(value3);
-	
-	$('#js-result').html(v1 + v2 + v3 + ' у.е.');
+
+	$('#js-result').html(v1 + v2 + v3 + ' у.е.'); 
+    console.log(v1, v2, v3)
 });
-let val1 = 0;
-let val2 = 0;
-let val3 = 0;
-
-$('#js-button').click(function(){
-	val1 = $('#siteR').val();
-	if ($('#select option[value=1]').prop('selected', true)){
-	val1 = 1;
-}
-	else if ($('#select option[value=2]').prop('selected', true)){
-	val1 = 2;
-}
-	else if ($('#select option[value=3]').prop('selected', true)){
-	val1 = 3;
-
-} 	else { 
-	val1 = 0;
-	console.log(val1);
-};
-
-
-$('#js-result2').html(val1 + val2 + val3 + ' д.');
-console.log(val1 + val2 + val3 + ' д.');
-
-let arrPrice = [
-[100, 200, 300],
-[50, 60, 70],
-[0, 10],
-];
- 
- let arrTiming = [
-[1, 2, 3],
-[0, 1, 3],
-[0, 1],
- ];
-
- 
-//animation number
- $(document).ready(function () {
- 
-    let show = true;
-    let countbox = ".benefits__inner";
-    $(window).on("scroll load resize", function () {
-        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
-        let w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
-        let e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
-        let w_height = $(window).height(); // Высота окна браузера
-        let d_height = $(document).height(); // Высота всего документа
-        let e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
-            $('.benefits__number').css('opacity', '1');
-            $('.benefits__number').spincrement({
-                thousandSeparator: "",
-                duration: 2000
-            });
-             
-            show = false;
-        }
-    });
- 
 });
-//animatiom number
 
-
-
-
-
-
-//анимация цифр
 $(document).ready(function(){
-    $('.benefits__number').spincrement({
-        thousandSeparator: "",
-        duration: 2000
-    });
+$('#js-button').click(function(){
+    let t = 0;
+
+    let value1 = $('#siteR').val();
+
+        if (value1 == '100'){
+            t = 1;
+        }
+        else if (value1 == '200'){
+            t = 2;
+        }
+        else if (value1 == '300'){
+            t = 3;
+        };
+       
+        console.log(t);
+
+//        
+    let value2 = $('#designR').val();
+        if (value2 == '50'){
+            t += 0;
+        }
+        else if (value2 == '60'){
+            t += 2;
+        }
+        else if (value2 == '70'){
+            t += 3;
+        };
+       
+        console.log(t)
+ //       
+    let value3 = $('#adaptabilityR').val();
+    
+        if (value3 == '0'){
+            t += 0;
+        }
+        else if (value3 == '10'){
+            t += 1;
+        };
+        console.log(t);
+
+if (t == 0){
+            alert("вы не выбрали пункт");
+        };
+        
+    $('#js-result2').html(t + ' д.');
+
 });
-//анимация цифр
+});
+
